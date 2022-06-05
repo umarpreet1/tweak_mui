f2fe4619cde36f8ee1362478a24c36c2d87d6ea2
Umarpreet Singh <umarpreet.singh@mail.utoronto.ca>
2022-06-04 21:33:38 -0400

    UI Setup resolve

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import ResponsiveAppBar from "./components/AppBar";
+import PrimaryNavigation from "./components/PrimaryNavigation";
+import SecondaryNavigation from "./components/SecondaryNavigation";
+
+import DashboardCore from "./components/DashboardCore";
+import Container from "@mui/material/Container";
+import './index.css';
+
+function App() {
+  return (<>
+      <ResponsiveAppBar/>
+            <Container >
+            <PrimaryNavigation />
+            <SecondaryNavigation />
+
+                </Container>
+      </>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
